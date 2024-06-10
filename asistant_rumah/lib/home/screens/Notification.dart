c077a9597a2cb87024fa8d20b59a5b798c5920f2
faisal adha <isaladha0@gmail.com>
2024-06-10 07:03:54 +0700

    update notification.dart

--- asistant_rumah/lib/home/screens/Notification.dart
+++ asistant_rumah/lib/home/screens/Notification.dart
@@ -1,38 +1,100 @@
 import 'package:flutter/material.dart';
 import 'package:asistant_rumah/home/widgets/text_widget.dart';
 
-class NotificationPage extends StatelessWidget {
+class NotificationPage extends StatefulWidget {
+  @override
+  _NotificationPageState createState() => _NotificationPageState();
+}
+
+class _NotificationPageState extends State<NotificationPage> {
+  String _selectedSortOption = 'Waktu'; // Opsi default
+
+  List<Map<String, String>> notifications = List.generate(
+    5,
+    (index) => {
+      'title': 'Notifikasi $index',
+      'detail': 'Detail notifikasi $index',
+    },
+  );
+
+  void _sortNotifications() {
+    setState(() {
+      if (_selectedSortOption == 'Waktu') {
+        // Sort by time, assuming the list is already in time order
+        notifications = notifications.reversed.toList();
+      } else if (_selectedSortOption == 'A-Z') {
+        // Sort by title A-Z
+        notifications.sort((a, b) => a['title']!.compareTo(b['title']!));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Notifikasi'),
       ),
-      body: ListView.builder(
-        itemCount: 5, // Jumlah notifikasi dummy
-        itemBuilder: (context, index) {
-          return ListTile(
-            leading: Icon(
-              Icons.notifications, // Icon notifikasi
-              color: Colors.blue, // Warna icon
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: DropdownButton<String>(
+              value: _selectedSortOption,
+              items: [
+                DropdownMenuItem(
+                  child: Text('Waktu'),
+                  value: 'Waktu',
+                ),
+                DropdownMenuItem(
+                  child: Text('A-Z'),
+                  value: 'A-Z',
+                ),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  _selectedSortOption = value!;
+                  _sortNotifications();
+                });
+              },
             ),
-            title: TextWidget(
-              'Notifikasi $index', // Judul notifikasi
-              18, // Ukuran teks
-              Colors.black, // Warna teks
-              FontWeight.normal, // Bobot teks
+          ),
+          Expanded(
+            child: ListView.builder(
+              padding: EdgeInsets.all(10.0),
+              itemCount: notifications.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.notifications,
+                      color: Colors.blue,
+                      size: 30.0,
+                    ),
+                    title: TextWidget(
+                      notifications[index]['title']!,
+                      18,
+                      Colors.black,
+                      FontWeight.bold,
+                    ),
+                    subtitle: TextWidget(
+                      notifications[index]['detail']!,
+                      14,
+                      Colors.grey,
+                      FontWeight.normal,
+                    ),
+                    trailing: Icon(Icons.arrow_forward_ios, color: Colors.grey),
+                    onTap: () {
+                      // Aksi saat notifikasi ditekan (jika diperlukan)
+                    },
+                  ),
+                );
+              },
             ),
-            subtitle: TextWidget(
-              'Detail notifikasi $index', // Detail notifikasi
-              14, // Ukuran teks
-              Colors.grey, // Warna teks
-              FontWeight.normal, // Bobot teks
-            ),
-            onTap: () {
-              // Aksi saat notifikasi ditekan (jika diperlukan)
-            },
-          );
-        },
+          ),
+        ],
       ),
     );
   }
